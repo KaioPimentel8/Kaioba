502040f70692082c2c4abbfabe1341ecc52843f4
KaioPimentel8 <159165485+KaioPimentel8@users.noreply.github.com>
2024-06-05 09:30:17 -0300

    sexologia

--- kaiobaflix/src/Pages/Filmes.jsx
+++ kaiobaflix/src/Pages/Filmes.jsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
+
 function Filmes() {
+
+    const[filmes, setFilmes] = useState([])
     
-    
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=7c572a9f5b3ba776080330d23bb76e1e')
+    .then(response => response.json())
+    .then(response => setFilmes(response.results))
+    .catch (error => console.log(error))
+
     return (  
         <>
         <h1>Filmes</h1>
+        <div className="listafilmes">
+        {
+            filmes.map(
+                filme => (
+                    <div className="cardname" key={filme.id}>
+                         <h1> {filme.title} </h1>
+                         <p>{filme.overview}</p>
+                    </div>
+                   
+                )
+            )
+        }
+        </div>
         </>
     );
 }
